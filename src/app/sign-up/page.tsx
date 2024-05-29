8b4edf6cc20b8de4a304dea38cace2546e2053f9
goggle555 <goggle555@fives.tech>
2024-05-29 15:09:06 +0900

    feat: /sign-upページを仮実装する

---
+++ src/app/sign-up/page.tsx
@@ -0,0 +1,13 @@
+import { signUp } from './actions'
+
+export default function SignUpPage() {
+  return (
+    <form>
+      <label htmlFor='email'>Email:</label>
+      <input id='email' name='email' type='email' required />
+      <label htmlFor='password'>Password:</label>
+      <input id='password' name='password' type='password' required />
+      <button formAction={signUp}>Sign Up</button>
+    </form>
+  )
+}
