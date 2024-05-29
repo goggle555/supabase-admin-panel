7bad7d5384157e64b2f271d4585cbbad84c4c735
goggle555 <goggle555@fives.tech>
2024-05-29 15:10:17 +0900

    feat: PKCEフローでセッションを作成するAPIを実装する

---
+++ src/app/api/auth/callback/route.ts
@@ -0,0 +1,16 @@
+import { createClient } from '@/utils/supabase/server'
+import { NextResponse } from 'next/server'
+
+export async function GET(request: Request) {
+  const requestUrl = new URL(request.url)
+  const code = requestUrl.searchParams.get('code')
+
+  if (code) {
+    const supabase = createClient()
+    await supabase.auth.exchangeCodeForSession(code)
+  }
+
+  const next = requestUrl.searchParams.get('next')
+
+  return NextResponse.redirect(`${next}?code=${code}` || requestUrl.origin)
+}
